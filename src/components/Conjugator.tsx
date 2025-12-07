e37f61a25205173dafd0d24f203653c2b419b8f5
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2025-12-07 17:58:31 -0600

    refactor: update Conjugator component with checkbox inputs and display conjugation data

--- src/components/Conjugator.tsx
+++ src/components/Conjugator.tsx
@@ -1,27 +1,45 @@
+import conjugations from "../assets/conjugations.json";
+
 export const Conjugator = () => {
+  console.log(conjugations);
   return (
     <main className="flex flex-col items-center pb-4">
       <h1 className="text-xl mb-5">Conjugator Quiz</h1>
       <form className="flex flex-col gap-5">
-        
-        <div className="flex gap-2">
-          <input type="radio" name="tense" value="present" id="present" />
+        <fieldset className="flex gap-2">
+          <legend>Past and/or Present</legend>
+          <input type="checkbox" name="tense" value="present" id="present" />
           <label htmlFor="present">Present</label>
-          <input type="radio" name="tense" value="past" id="past" />
+          <input type="checkbox" name="tense" value="past" id="past" />
           <label htmlFor="past">Past</label>
-        </div>
+        </fieldset>
 
-        <div className="flex gap-2">
-          <input type="radio" name="def" value="definite" id="definite" />
+        <fieldset className="flex gap-2">
+          <legend>Definite and/or Indefinite</legend>
+          <input type="checkbox" name="def" value="definite" id="definite" />
           <label htmlFor="definite">Definite</label>
-          <input type="radio" name="def" value="indefinite" id="indefinite" />
+          <input
+            type="checkbox"
+            name="def"
+            value="indefinite"
+            id="indefinite"
+          />
           <label htmlFor="indefinite">Indefinite</label>
-        </div>
+        </fieldset>
 
         <button type="submit" className="border p-2">
           Start Quiz
         </button>
       </form>
+
+      <p>{conjugations[0].infinitive}</p>
+      <p>{conjugations[0].translation}</p>
+      {Object.keys(conjugations[0].present.indefinite).map((key) => (
+        <p className="p-2">
+          {key} <input className="border" />
+        </p>
+      ))}
+      <button className="border p-2">Submit</button>
     </main>
   );
 };
