e849fd98622ec7709f52d3f69ec8589cd7fa3cec
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2026-01-11 21:08:58 -0600

    Refactor FlashCard component to improve structure and add button group for ratings

--- src/components/FlashCard.tsx
+++ src/components/FlashCard.tsx
@@ -17,40 +17,46 @@ export const FlashCard = () => {
 
   return (
     <>
-    <CreateFlashCard/>
-    <div
-      className="w-full h-96 sm:max-w-md overflow-hidden rounded-lg border p-6"
-      onClick={() => handleFlip()}
-    >
-      {isFlipped ? (
-        <div className="flex flex-col items-center justify-between h-full gap-5">
-          <h2 className="text-3xl tracking-wide text-center text-slate-600">
-            {word}
-          </h2>
-          <div className="flex-1 w-full min-h-0 flex items-center justify-center">
-            {isLoading ? (
-              <Skeleton className="max-w-full max-h-full w-full h-full rounded-lg shadow-sm object-contain" />
-            ) : (
-              <img
-                className="max-w-full max-h-full object-contain rounded-lg shadow-sm"
-                src={image}
-                alt={word}
-              ></img>
-            )}
+      <CreateFlashCard />
+      <div
+        className="w-full h-96 sm:max-w-md overflow-hidden rounded-lg border p-6"
+        onClick={() => handleFlip()}
+      >
+        {isFlipped ? (
+          <div className="flex flex-col items-center justify-between h-full gap-5">
+            <h2 className="text-3xl tracking-wide text-center text-slate-600">
+              {word}
+            </h2>
+            <div className="flex-1 w-full min-h-0 flex items-center justify-center">
+              {isLoading ? (
+                <Skeleton className="max-w-full max-h-full w-full h-full rounded-lg shadow-sm object-contain" />
+              ) : (
+                <img
+                  className="max-w-full max-h-full object-contain rounded-lg shadow-sm"
+                  src={image}
+                  alt={word}
+                ></img>
+              )}
+            </div>
+            <ButtonGroup>
+              {ratings.map((rating) => (
+                <Button
+                  variant="outline"
+                  size="sm"
+                  key={rating}
+                  className="cursor-pointer"
+                >
+                  {rating}
+                </Button>
+              ))}
+            </ButtonGroup>
           </div>
-          <ButtonGroup>
-            {ratings.map((rating) => (
-              <Button variant="outline" size="sm" key={rating} className='cursor-pointer'>
-                {rating}
-              </Button>
-            ))}
-          </ButtonGroup>
-        </div>
-      ) : (
-        <div className="flex h-full w-fullflex items-center justify-center cursor-pointer">
-          <h2 className="text-7xl tracking-wide">{word}</h2>
-        </div>
-      )}
-    </div></>
+        ) : (
+          <div className="flex h-full w-fullflex items-center justify-center cursor-pointer">
+            <h2 className="text-7xl tracking-wide">{word}</h2>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
