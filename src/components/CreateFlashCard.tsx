daf3c4d797877145cd414b99bd2ab375b64d93bb
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2026-01-17 20:55:08 -0600

    upload image to storage bucket

--- src/components/CreateFlashCard.tsx
+++ src/components/CreateFlashCard.tsx
@@ -5,6 +5,7 @@ import { useForm } from "@tanstack/react-form";
 import { Button } from "./ui/button";
 import { useEffect, useMemo, useRef, useState } from "react";
 import { PageTitle } from "./PageTitle";
+import { supabase } from "@/lib/supabase";
 
 export const CreateFlashCard = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
@@ -14,14 +15,21 @@ export const CreateFlashCard = () => {
     defaultValues: {
       word: "",
     },
-    onSubmit:async({value})=>{
+    onSubmit: async ({ value }) => {
       setErrMsg(null)
       if (!selectedFile) {
-setErrMsg('Upload an image')
-return
+        setErrMsg('Upload an image')
+        return
       }
-      if (!value.word || value.word.trim()===''){
+      if (!value.word || value.word.trim() === '') {
         setErrMsg('Enter a word')
+        return
+      }
+      const uuid = crypto.randomUUID()
+      const result = await supabase.storage.from('cardimages').upload(`${value.word}-${uuid}`, selectedFile)
+      console.log(result)
+      if (result.error) {
+        setErrMsg('Image upload failed') 
         return
       }
     }
@@ -42,77 +50,76 @@ return
     }
   };
 
-  const imageUrl=useMemo(()=>{
-    if(!selectedFile) return null
+  const imageUrl = useMemo(() => {
+    if (!selectedFile) return null
     return URL.createObjectURL(selectedFile)
-  },[selectedFile])
+  }, [selectedFile])
 
   useEffect(() => {
-      return () => {
-        if (imageUrl)         URL.revokeObjectURL(imageUrl);
-      };
-
+    return () => {
+      if (imageUrl) URL.revokeObjectURL(imageUrl);
+    };
   }, [imageUrl]);
   return (
     <>
-    <PageTitle title="Create Flash Cards" />
+      <PageTitle title="Create Flash Cards" />
 
-    <div className="w-full h-96 sm:max-w-md overflow-hidden rounded-lg border p-6 flex flex-col justify-between items-center">
-      <div className="w-full">
-        <form.Field
-          name="word"
-          children={(field) => (
-            <FieldGroup className="gap-2">
-              <FieldLabel htmlFor={field.name} className="self-center text-md">
-                Enter Word
-              </FieldLabel>
-              <Input
-                id={field.name}
-                name={field.name}
-                value={field.state.value}
-                onBlur={field.handleBlur}
-                onChange={(e) => field.handleChange(e.target.value)}
-                placeholder="kutya"
-                className="text-xl text-center border shadow-none focus-visible:ring-0 placeholder:text-slate-400 w-2/3 self-center"
-              />
-              {field.state.meta.errors ? (
-                <em className="text-[10px] text-red-500">
-                  {field.state.meta.errors.join(",")}
-                </em>
-              ) : null}
-            </FieldGroup>
-          )}
+      <div className="w-full h-96 sm:max-w-md overflow-hidden rounded-lg border p-6 flex flex-col justify-between items-center">
+        <div className="w-full">
+          <form.Field
+            name="word"
+            children={(field) => (
+              <FieldGroup className="gap-2">
+                <FieldLabel htmlFor={field.name} className="self-center text-md">
+                  Enter Word
+                </FieldLabel>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  placeholder="kutya"
+                  className="text-xl text-center border shadow-none focus-visible:ring-0 placeholder:text-slate-400 w-2/3 self-center"
+                />
+                {field.state.meta.errors ? (
+                  <em className="text-[10px] text-red-500">
+                    {field.state.meta.errors.join(",")}
+                  </em>
+                ) : null}
+              </FieldGroup>
+            )}
+          />
+        </div>
+
+        {imageUrl ? (
+          <img
+            src={imageUrl}
+            alt="selected image"
+            className="w-full max-h-48 object-contain cursor-pointer"
+            onClick={handleBoxClick}
+          />
+        ) : (
+          <Button
+            className="border-dashed border-black flex-1 m-8 w-56"
+            variant="outline"
+            size="icon-lg"
+            onClick={handleBoxClick}
+          >
+            <ImagePlus />
+          </Button>
+        )}
+
+        <input
+          type="file"
+          ref={fileInputRef}
+          onChange={handleFileChange}
+          className="hidden"
+          accept="image/*"
         />
+        <p className="text-xs">{errMsg}</p>
+        <Button onClick={() => form.handleSubmit()}>Create Card</Button>
       </div>
-
-      {imageUrl ? (
-        <img
-          src={imageUrl}
-          alt="selected image"
-          className="w-full max-h-48 object-contain cursor-pointer"
-          onClick={handleBoxClick}
-        />
-      ) : (
-        <Button
-          className="border-dashed border-black flex-1 m-8 w-56"
-          variant="outline"
-          size="icon-lg"
-          onClick={handleBoxClick}
-        >
-          <ImagePlus />
-        </Button>
-      )}
-
-      <input
-        type="file"
-        ref={fileInputRef}
-        onChange={handleFileChange}
-        className="hidden"
-        accept="image/*"
-      />
-<p className="text-xs">{errMsg}</p>
-      <Button onClick={()=>form.handleSubmit()}>Create Card</Button>
-    </div>
     </>
   );
 };
